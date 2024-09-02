30be8c8288d235ba9d6b76c91c92f38982c27b97
Asm Shad (INT) <asm.shad@liz.com>
2024-09-02 08:46:01 +0600

    Offer Section with complex Grid layout Complete

--- js/app.js
+++ js/app.js
@@ -6,3 +6,15 @@ $(document).ready(function(){
         fade: true
     });
 });
+
+
+const triggers = document.querySelectorAll('.menu-trigger');
+
+console.log(triggers);
+
+for (let i = 0; i < triggers.length; i++){
+    triggers[i].addEventListener('click', function(e){
+        e.preventDefault();
+        this.classList.toggle('is-open');
+    });
+}
